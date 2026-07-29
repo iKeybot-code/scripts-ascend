b8058c93a4e1e59bd1bfa03819394efddf745b6c
l00848175 <l00848175@huawei.com>
2026-07-30 01:00:25 +0800

    prepare_aisbench

--- prepare_aisbench.sh
+++ prepare_aisbench.sh
@@ -1,18 +1,23 @@
-cd /workspace
-if [ -d aisbench/.git ]; then
-  cd aisbench && git fetch --all && git pull --ff-only
+WORK_SPACE="/mnt/a800_share/l00848175/workspace"
+BENCH_MARK="benchmark"
+if [ -d "${BENCH_MARK}"/.git ]; then
+  cd "${BENCH_MARK}" && git fetch --all && git pull --ff-only
 else
-  git clone https://github.com/AISBench/benchmark.git aisbench
-  cd aisbench
+  git clone https://github.com/AISBench/benchmark.git "${BENCH_MARK}"
+  cd "${BENCH_MARK}"
 fi
 
-pip install -e .
+# 从源码安装 AISBench
+pip3 install -e ./ --use-pep517
+# 安装额外的 AISBench 依赖
+pip3 install -r requirements/api.txt
+pip3 install -r requirements/extra.txt
 
 # AIME2025 数据路径（评测脚本硬性检查）
-mkdir -p ais_bench/datasets/aime2025
+mkdir -p "${BENCH_MARK}"/datasets/aime2025
 # 将数据放到：
-#   /workspace/aisbench/ais_bench/datasets/aime2025/aime2025.jsonl
-wc -l /workspace/aisbench/ais_bench/datasets/aime2025/aime2025.jsonl
+#   /"${WORK_SPACE}"/"${BENCH_MARK}"/ais_bench/datasets/aime2025/aime2025.jsonl
+wc -l /"${WORK_SPACE}"/"${BENCH_MARK}"/ais_bench/datasets/aime2025/aime2025.jsonl
 # 历史环境为 30 条
 
 python3 -c "import ais_bench; print(ais_bench.__file__)"
